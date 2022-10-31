da7c77ae00f423900522a67c5078ec0d1a63533b
YsDnG <ya.denguir@gmail.com>
2022-10-31 22:20:51 +0100

    Add ui for Rock Paper Scissor Project 	modified:   index.html 	modified:   scipt.js 	modified:   style.css

--- scipt.js
+++ scipt.js
@@ -1,78 +1,98 @@
     const choice=["Paper","Rock","Scissor"]
-    const buttonStart = document.getElementById("startButton")
-    const divResult= document.getElementById('result')
-    console.log(divResult)
-    divResult.innerText = "Are you Ready to Rumble ?? "
-    const buttonChoiceContainer = document.getElementById('containerButtonChoice')
+    const buttonStart = document.querySelector(".buttonGame")
+    const divResult= document.querySelector('.dynamic-txts')
+    let p = document.createElement("p");
+    
+    const buttonChoiceContainer = document.querySelector('.button-container')
 
     buttonChoiceContainer.style.display='none'
     let nbGame = 0
     let result = 0
-    let score = [ 0 ,0, 0]
+    let score = [ 0 ,0]
+    const buttonChoice = buttonChoiceContainer.querySelectorAll('.buttonChoice')
+    let playerChoice= null
+    
     
 
-    
 
-
-
-
-function playAGame(e){ 
-   
- 
-    const buttonChoice = buttonChoiceContainer.querySelectorAll('button')
-    
-    if(buttonStart.innerHTML === "Start")
-    {
-        buttonChoiceContainer.style.display='flex'
-        buttonStart.innerHTML = "Reset"
-        buttonStart.classList.add('resetButton') 
-        
-        
-        const computerChoice = Math.round(Math.random()*2)
-
-        
-
-
-        function setPlayerChoice(e) 
-        { 
-            playerChoice = this.innerHTML.toLowerCase()
-            result += getGameWinner(choice[computerChoice].toLowerCase(),playerChoice)
-            nbGame++
-            playerChoice = null
-            
-
-            if(nbGame > 3)
+function setPlayerChoice(e)
+{
+    playerChoice = this.innerHTML.toLowerCase()
+    const computerChoice = Math.round(Math.random()*2)
+    resetAnimation()
+    result += getGameWinner(choice[computerChoice].toLowerCase(),playerChoice)
+    nbGame++
+    playerChoice = null
+    if(nbGame > 5)
             {
              buttonChoice.forEach(button => button.removeEventListener('click',setPlayerChoice))
                     
             if(result !== 0)
             {
                 if(result > 0)
-                    divResult.innerText = "You Win  THE GAME" 
+                {
+                divResult.firstElementChild.classList.remove("loose")
+                divResult.firstElementChild.classList.add("win") 
+                divResult.firstElementChild.innerText = ("You Win  THE GAME with " + score[0]+" V and "+ score[1]+' L').toLocaleUpperCase() 
+            }
                 else
-                    divResult.innerText ="You Loose  THE GAME"
+                {
+                    divResult.firstElementChild.classList.remove("win")
+                    divResult.firstElementChild.classList.add("loose")
+                    divResult.firstElementChild.innerText =("You Loose  THE GAME "+ score[1] +' L ' +"to " +score[0]+' V').toUpperCase()
+                }
 
             }
             else
-                divResult.innerText ="THE GAME IS  A TIE "
+                divResult.firstElementChild.innerText =("THE GAME IS  A TIE "+score[1]+" point(s) for each ").toUpperCase()
 
         
+                buttonStart.innerHTML = "PLAY AGAIN"
             }
-            
-        }
-        buttonChoice.forEach(button => button.addEventListener('click', setPlayerChoice))
-            
+           
 
+}
+
+function resetAnimation()
+{
+    divResult.removeChild(divResult.firstChild)
+    divResult.append(p)
+    divResult.firstElementChild.style="none"
+    requestAnimationFrame(()=>
+    {
+        divResult.firstElementChild.style="";
+    })
+    setTimeout(() => {
+        
+    }, 1500);
+}
+
+function StartAGame(e){ 
+
+   
+    resetAnimation()
+
+    if(buttonStart.innerHTML === "START")
+    {
+        buttonChoiceContainer.style.display='flex'
+        buttonStart.innerHTML = "RESET"
+        buttonStart.classList.add('resetButton') 
+   
+        divResult.firstElementChild.innerText = "THE MATCH STARTED !".toLocaleUpperCase() 
+        buttonChoice.forEach(button => button.addEventListener('click', setPlayerChoice))
     }
     else
     {
-        buttonStart.innerHTML = "Start" 
+        buttonStart.innerHTML = "START" 
         buttonChoiceContainer.style.display='none'
         buttonStart.classList.remove('resetButton') 
-        divResult.innerText = "Are you Ready to Rumble ??"
-        divResult.classList.remove('loose')
+        divResult.firstElementChild.classList.remove("win")
+        divResult.firstElementChild.classList.remove("loose")
+        divResult.firstElementChild.classList.add("white")
+        divResult.firstElementChild.innerText="ARE YOU READY ?"
         result = 0
         nbGame = 0
+        score= [0,0]
         
     }
 }
@@ -80,45 +100,66 @@ function playAGame(e){
 
  function getGameWinner(cC ,pC)
 {
+
     if(cC === pC)
     { 
-        divResult.innerText ="It's a tie : " + cC
+        
+        divResult.firstElementChild.classList.remove("win")
+        divResult.firstElementChild.classList.remove("loose")
+        divResult.firstElementChild.classList.add("white")
+        divResult.firstElementChild.innerText=("It is a tie : " + cC).toUpperCase()
         return 0
     }  
     if(cC=== "paper" &&  pC === "rock")
     { 
-        divResult.innerText ="You Lose!  Paper beats Rock"
+        divResult.firstElementChild.classList.remove("win")
+        divResult.firstElementChild.classList.add("loose")
+        divResult.firstElementChild.innerText ="You Loose !  Paper beat Rock".toUpperCase()
+        score[1]++
         return -1
     }
     if( cC=== "rock" &&  pC === "paper")
     { 
-        divResult.innerText ="You Win!  Paper beats  Rock" 
-        
+        divResult.firstElementChild.classList.remove("loose")
+        divResult.firstElementChild.classList.add("win")
+        divResult.firstElementChild.innerText="You Win !  Paper beat  Rock".toUpperCase() 
+        score[0]++
         return 1  
     }
     if( cC=== "paper" &&  pC === "scissor")
     { 
-        divResult.innerText ="You Win! Scissor beats  Paper"
+        divResult.firstElementChild.classList.remove("loose")
+        divResult.firstElementChild.classList.add("win")
+        divResult.firstElementChild.innerText ="You Win ! Scissor beat  Paper".toUpperCase()
+        score[0]++
         return 1 
     }
     if( cC=== "scissor" && pC === "paper")
     { 
-        divResult.innerText ="You Loose! Scissor beats  Paper"
+        divResult.firstElementChild.classList.remove("win")
+        divResult.firstElementChild.classList.add("loose")
+        divResult.firstElementChild.innerText="You Loose ! Scissor beat  Paper".toUpperCase()
+        score[1]++
         return -1
     }
     if(cC=== "rock" && pC === "scissor")
     { 
-        divResult.innerText ="You Loose! Rock beats scissor"
+        divResult.firstElementChild.classList.remove('all')
+        divResult.firstElementChild.classList.add("loose")
+        divResult.firstElementChild.innerText ="You Loose ! Rock beat scissor".toUpperCase()
+        score[1]++
         return -1
     }
         if(cC=== "scissor" && pC === "rock")
     { 
-        divResult.innerText ="You Win! Rock beats Scissor"
+        divResult.firstElementChild.classList.remove("loose")
+        divResult.firstElementChild.classList.add("win")
+        divResult.firstElementChild.innerText="You Win ! Rock beat Scissor".toUpperCase()
+        score[0]++
         return 1 
     }
 
 
 
 }
-
-buttonStart.addEventListener("click",playAGame)
+buttonStart.addEventListener("click",StartAGame)
